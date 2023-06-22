43ee4e8bee2e82ca53ae1e1389938ea36f1257c2
Mokytojas <Mokytojas@kitm.local>
2023-06-22 11:22:29 +0300

    first commit

---
+++ frontend/src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/authSlice';
+import goalReducer from '../features/goalSlice';
+
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    goals: goalReducer
+  },
+});
